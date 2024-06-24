51b5d8720f5672a66e7c2c105d569136086214c5
Eduardo Andrade Ferreira <135661746+WizEDPill@users.noreply.github.com>
2024-06-24 16:34:20 -0300

    banco1.0.rep
    modificação com as alterações de repetição

--- banco.py
+++ banco.py
@@ -1,11 +1,12 @@
 total=0
+contador=0
 while True:
     valor = int(input('Informe o valor a ser Depositado ='))
     total+=valor
 
     contint= input('voce deseja continuar depositando ? y-sim n- nao')
     if contint=='n':
-        sq=input('deseja ver extrado? y-sim n-nao')
+        sq=input('deseja ver extrado? y-sim n-nao').strip().lower()
         if sq=='n':
             break;
         if sq=='y':
@@ -15,16 +16,23 @@ while True:
                 break;
             if numb=='y': 
                 saq= int(input('Informe o valor do saque: '))
+                print('O Limite são 3 saques diarios de no maximo R$500reais ')
+                mumb=+saq
                 if saq>total: 
-                    print(f'seu saldo e R$ {saq-total} a menos que seu valor em conta, assim não sera possivel fazer o saque.')
+                        print(f'seu saldo e R$ {saq-total} a menos que seu valor em conta, assim não sera possivel fazer o saque.')
+                elif saq>500: 
+                        print('Valor de saque excede o limite de R$500,00 por saque')
+                        continue;
+                elif contador>3: 
+                        print('voce ultrapassou o limite de saques diarios')
                 else: 
-                    print(f'seu saque de R${saq} foi feito com sucesso seu extrato agora é de R$ {total-saq} ')
-                    valor= input('Deseja voltar o menu de deposito ou concluir a operacao?y- sim ,n- nao')
-                    if valor=='y': 
-                        continue; 
-                    else: 
-                        break; 
-
-            
-                
+                        print(f'seu saque de R${saq} foi feito com sucesso seu extrato agora é de R$ {total-saq} ')
+                        contador+=1
+                        valor= input('Deseja voltar o menu de deposito ou concluir a operacao?y- sim ,n- nao')
+                if valor=='y': 
+                            continue; 
+                else: 
+                            break; 
+                    
+print(f'O valor total depositado é R${total}.')
 
