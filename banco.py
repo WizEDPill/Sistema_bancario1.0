d80d82c153f829b87f3f2e981f89bc89eb654e83
Eduardo Andrade Ferreira <135661746+WizEDPill@users.noreply.github.com>
2024-07-10 21:05:04 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,38 +1,37 @@
-total=0
-contador=0
+total = 0
+counter = 0
 while True:
-    valor = int(input('Informe o valor a ser Depositado ='))
-    total+=valor
+    amount = int(input('Enter the amount to be deposited: '))
+    total += amount
 
-    contint= input('voce deseja continuar depositando ? y-sim n- nao')
-    if contint=='n':
-        sq=input('deseja ver extrado? y-sim n-nao').strip().lower()
-        if sq=='n':
-            break;
-        if sq=='y':
-            print(f'O valor do seu extrato: ------- R${total}-------')
-            numb=input('Voce deseja fazer o saque de algum valor ? y-sim n- nao')
-            if numb=='n':
-                break;
-            if numb=='y': 
-                saq= int(input('Informe o valor do saque: '))
-                print('O Limite são 3 saques diarios de no maximo R$500reais ')
-                mumb=+saq
-                if saq>total: 
-                        print(f'seu saldo e R$ {saq-total} a menos que seu valor em conta, assim não sera possivel fazer o saque.')
-                elif saq>500: 
-                        print('Valor de saque excede o limite de R$500,00 por saque')
-                        continue;
-                elif contador>3: 
-                        print('voce ultrapassou o limite de saques diarios')
-                else: 
-                        print(f'seu saque de R${saq} foi feito com sucesso seu extrato agora é de R$ {total-saq} ')
-                        contador+=1
-                        valor= input('Deseja voltar o menu de deposito ou concluir a operacao?y- sim ,n- nao')
-                if valor=='y': 
-                            continue; 
-                else: 
-                            break; 
+    contint = input('Do you want to continue depositing? y-yes n-no: ').strip().lower()
+    if contint == 'n':
+        sq = input('Do you want to see the statement? y-yes n-no: ').strip().lower()
+        if sq == 'n':
+            break
+        if sq == 'y':
+            print(f'Your statement balance is: ------- ${total} -------')
+            withdraw_decision = input('Do you want to make a withdrawal? y-yes n-no: ').strip().lower()
+            if withdraw_decision == 'n':
+                break
+            if withdraw_decision == 'y':
+                withdrawal_amount = int(input('Enter the amount to withdraw: '))
+                print('The limit is 3 withdrawals per day with a maximum of $500 per withdrawal.')
+                total_withdrawn = withdrawal_amount
+                if withdrawal_amount > total:
+                    print(f'Your balance is ${withdrawal_amount - total} less than the amount in your account, so the withdrawal is not possible.')
+                elif withdrawal_amount > 500:
+                    print('Withdrawal amount exceeds the $500.00 per withdrawal limit.')
+                    continue
+                elif counter >= 3:
+                    print('You have exceeded the daily withdrawal limit.')
+                else:
+                    print(f'Your withdrawal of ${withdrawal_amount} was successful. Your new balance is ${total - withdrawal_amount}.')
+                    counter += 1
+                    back_to_menu = input('Do you want to return to the deposit menu or conclude the operation? y-yes n-no: ').strip().lower()
+                    if back_to_menu == 'y':
+                        continue
+                    else:
+                        break
                     
-print(f'O valor total depositado é R${total}.')
-
+print(f'The total amount deposited is ${total}.')
